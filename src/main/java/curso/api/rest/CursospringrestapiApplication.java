128fb0c64ae9542e8c8e8a77f98478fee19a0df0
Rodrigo Xavier <xavier.rodrigofabiano@gmail.com>
2020-05-06 17:10:27 -0300

    commit referente a utilizacao de cache para uma busca com melhor peformance

--- src/main/java/curso/api/rest/CursospringrestapiApplication.java
+++ src/main/java/curso/api/rest/CursospringrestapiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -21,24 +21,37 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @RestController
 @EnableAutoConfiguration
+@EnableCaching
 public class CursospringrestapiApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringrestapiApplication.class, args);
-		System.out.println(new BCryptPasswordEncoder().encode("123"));
+		//System.out.println(new BCryptPasswordEncoder().encode("123"));
 	}
 	
-	//Mapeamento global que refletem em todo sistema
+    /*Mapeamento Global que refletem em todo o sistema*/
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/usuario/**").allowedMethods("*").allowedOrigins("*");//Liberando o mapeamento de usuario para todas as origens
+		
+		registry.addMapping("/usuario/**")
+		.allowedMethods("*")
+		.allowedOrigins("*");
+		/*Liberando o mapeamento de usuario para todas as origens*/
 		//registry.addMapping("*/**");//Com esta anotacao esta dando acesso a todos os controler e end-point
 		//registry.addMapping("*/usuario/**");//Com esta anotacao esta dando acesso a tudo do /usuario
 		//registry.addMapping("*/usuario/**").allowedMethods("*");//Com esta anotacao esta liberando todos os metodos do /usuario
 		//registry.addMapping("*/usuario/**").allowedMethods("POST","PUT","DELETE");//Com esta anotacao esta dando acesso a um determinado metodo
 		//registry.addMapping("*/usuario/**").allowedMethods("POST").allowedOrigins("www.teste.com.br","www.teste2.com.br");//Com esta anotacao esta dando acesso apenas ao metodo post e vindo de uma determinada url ou varias
 		//Na aula configuracao centralizado do mudolo 36 tem um exemplo de liberar varios metodos e varias origens
-	}	
+		
+	}
 
 }
+
+
+
+
+
+
+
 
--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,14 +1,15 @@
 package curso.api.rest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,101 +17,138 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import curso.api.rest.model.Usuario;
 import curso.api.rest.repositoy.UsuarioRepository;
 
-/*Para liberar apenas um End-Point retira essa anotacao*/
-//@CrossOrigin(origins = "*")/*apenas a anotacao CrossOrigin por padrao libera tudo*/
-/*pode se passar um array de url ex: origin={"teste.com.br", teste2.com.br}*/
-@RestController /*Arquitetura Rest*/
+@RestController /* Arquitetura REST */
 @RequestMapping(value = "/usuario")
 public class IndexController {
 	
-	@Autowired
-	UsuarioRepository usuarioRepository;
+	@Autowired /* se fosse CDI seria @Inject*/
+	private UsuarioRepository usuarioRepository;
 	
-	//Passando duas variaveis na url
-	@GetMapping(value = "/{id}/codigoVenda{venda}", produces = "application/pdf")
-	public ResponseEntity<Usuario> relatorio(@PathVariable(value = "id") Long id, 
-			@PathVariable(value="venda") Long venda) {
+	
+	/* Serviço RESTful */
+	@GetMapping(value = "/{id}/codigovenda/{venda}", produces = "application/json")
+	public ResponseEntity<Usuario> relatorio(@PathVariable (value = "id") Long id
+			                                , @PathVariable (value = "venda") Long venda) {
 		
 		Optional<Usuario> usuario = usuarioRepository.findById(id);
 		
-		return new ResponseEntity(usuario.get(), HttpStatus.OK);
+		/*o retorno seria um relatorio*/
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
 	}
 	
-	@GetMapping(value = "v1/{id}", produces = "application/json")/*versionamento pela uri*/
-	public ResponseEntity<Usuario> initV1(@PathVariable(value = "id") Long id) {
+
+	/* Serviço RESTful */
+	@GetMapping(value = "/{id}", produces = "application/json")
+	@Cacheable("cacheuser")
+	public ResponseEntity<Usuario> init(@PathVariable (value = "id") Long id) {
 		
 		Optional<Usuario> usuario = usuarioRepository.findById(id);
-		System.out.println("Executando versão 1");
-		return new ResponseEntity(usuario.get(), HttpStatus.OK);
-	}
-	
-	@GetMapping(value = "/{id}", produces = "application/json", headers="X-API-version=v2")/*versionamento pelo header*/
-	public ResponseEntity<Usuario> initV2(@PathVariable(value = "id") Long id) {
 		
-		Optional<Usuario> usuario = usuarioRepository.findById(id);
-		System.out.println("Executando versão 2");
-		return new ResponseEntity(usuario.get(), HttpStatus.OK);
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
 	}
 	
 	@DeleteMapping(value = "/{id}", produces = "application/text")
-	public String deletar(@PathVariable(value = "id") Long id) {
+	public String delete (@PathVariable("id") Long id){
 		
 		usuarioRepository.deleteById(id);
-		return "Deletado com Sucesso!";
-				
+		
+		return "ok";
 	}
-	/*para deixar acesso a todos basta deixar apenas a anotacao @CrossOrigin*/
-	//@CrossOrigin(origins = "so vai acessar quem vier desta url www.teste.com.br")
-	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<List<Usuario>> usuario(){
 	
+	
+	@DeleteMapping(value = "/{id}/venda", produces = "application/text")
+	public String deletevenda(@PathVariable("id") Long id){
+		
+		usuarioRepository.deleteById(id);
+		
+		return "ok";
+	}
+	
+
+	/*Vamos supor que o carregamento de usuário seja um processo lento
+	 * e queremos controlar ele com cache para agilizar o processo*/
+	@GetMapping(value = "/", produces = "application/json")
+	@Cacheable("cacheusuarios")
+	public ResponseEntity<List<Usuario>> usuario () throws InterruptedException{
+		
 		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+		
+		Thread.sleep(6000);/*Segura o codigo por 6 segunos simulando um processo lento*/
+
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
 	
-	//@CrossOrigin(origins = "localhost:8080")
-	@PutMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
-
-		for(int pos=0; pos < usuario.getTelefones().size(); pos++) {
-			usuario.getTelefones().get(pos).setUsuario(usuario);
-		}
-		
-		Usuario userTemporario = usuarioRepository.findUserByLogin(usuario.getLogin());
-		if(!userTemporario.getLogin().equals(usuario.getLogin())) { /*Senhas diferentes*/
-			String senhaCriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
-			usuario.setSenha(senhaCriptografada);
-		}
-		
-		Usuario usuarioAlterado = usuarioRepository.save(usuario);
-		return new ResponseEntity<Usuario>(usuarioAlterado, HttpStatus.OK);
-	}
 	
 	@PostMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+	public ResponseEntity<Usuario> cadastrar(@RequestBody @Valid Usuario usuario) {
 		
-		for(int pos=0; pos < usuario.getTelefones().size(); pos++) {
+		for (int pos = 0; pos < usuario.getTelefones().size(); pos ++) {
 			usuario.getTelefones().get(pos).setUsuario(usuario);
 		}
 		
-		String senhaCriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
-		usuario.setSenha(senhaCriptografada);
+		String senhacriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
+		usuario.setSenha(senhacriptografada);
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
 		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 		
 	}
 	
-	@PostMapping(value = "/{idUser}/idVenda{idVenda}", produces = "application/json")
-	public ResponseEntity<Usuario> cadastrarVenda(@PathVariable Long idUser, @PathVariable Long idVenda){
+	
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
 		
-		//Usuario usuarioCadastrado = usuarioRepository.save(usuario);
-		return new ResponseEntity("Id usuario: " + idUser + "Id Venda: " + idVenda, HttpStatus.OK);
+		/*outras rotinas antes de atualizar*/
+		
+		for (int pos = 0; pos < usuario.getTelefones().size(); pos ++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+		
+		Usuario userTemporario = usuarioRepository.findUserByLogin(usuario.getLogin());
+		
+		
+		if (!userTemporario.getSenha().equals(usuario.getSenha())) { /*Senhas diferentes*/
+			String senhacriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
+			usuario.setSenha(senhacriptografada);
+		}
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
 	}
+	
+	
+	
+	@PutMapping(value = "/{iduser}/idvenda/{idvenda}", produces = "application/json")
+	public ResponseEntity updateVenda(@PathVariable Long iduser, 
+			                                     @PathVariable Long idvenda) {
+		/*outras rotinas antes de atualizar*/
+		
+		//Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity("Venda atualzada", HttpStatus.OK);
+		
+	}
+	
+	
+	@PostMapping(value = "/{iduser}/idvenda/{idvenda}", produces = "application/json")
+	public ResponseEntity cadastrarvenda(@PathVariable Long iduser, 
+			                                     @PathVariable Long idvenda) {
+		
+		/*Aqui seria o processo de venda*/
+		//Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity("id user :" + iduser + " idvenda :"+ idvenda, HttpStatus.OK);
+		
+	}
+	
+	
+	
+
 }
